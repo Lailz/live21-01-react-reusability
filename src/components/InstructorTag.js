0844a53ebd5be77b13603de98ef00ece5f0c7874
Lailz <lailaabdulraheem@gmail.com>
2021-01-24 09:39:02 +0300

    finished

--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,7 +1,15 @@
-const InstructorTag = () => {
+import { Emoji, GoToGithub, Name, TagWrapper } from "./styles";
+
+const InstructorTag = ({ github, name, emoji }) => {
   // Create the InstructorTag component here
 
-  return <></>;
+  return (
+    <TagWrapper onClick={() => window.open(`https://github.com/${github}`)}>
+      <Emoji>{emoji}</Emoji>
+      <Name>{name}</Name>
+      <GoToGithub>Go to GitHub</GoToGithub>
+    </TagWrapper>
+  );
 };
 
 export default InstructorTag;
